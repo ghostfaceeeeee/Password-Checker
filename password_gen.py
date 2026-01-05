65488e9b0c2218d66b42780cc3e829bd18a05570
whaleafs <asakura1198@gmail.com>
2026-01-05 11:01:46 +0700

    Delete password_gen.py

--- password_gen.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import secrets, string
-
-def password_generator():
-    length = st.slider("Length", 8, 32, 16)
-    if st.button("Generate"):
-        chars = string.ascii_letters + string.digits
-        st.success(''.join(secrets.choice(chars) for _ in range(length)))
