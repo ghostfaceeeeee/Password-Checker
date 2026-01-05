46c3890eea5c14e18548d0a4e4c2fb4f61ca90b4
whaleafs <asakura1198@gmail.com>
2026-01-05 11:01:39 +0700

    Delete password_check.py

--- password_check.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from zxcvbn import zxcvbn
-
-def password_checker():
-    pwd = st.text_input("Password", type="password")
-    if pwd:
-        result = zxcvbn(pwd)
-        st.write("Score:", result["score"])
